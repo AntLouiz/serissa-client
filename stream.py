8b6cb08126709fea6edb78188e64c76f306e6d56
AntLouiz <luizrodrigo46@hotmail.com>
2020-02-28 11:18:16 -0300

    fixing the photo cropping

--- stream.py
+++ stream.py
@@ -9,6 +9,7 @@ from cache import redis_instance
 def stream_faces(ws):
     detection_method = 'hog'
     cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 20)
 
     cap.set(3, 320)
     cap.set(4, 240)
@@ -27,7 +28,7 @@ def stream_faces(ws):
         if len(boxes):
             for (x, w, h, y) in boxes:
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img = img[y:y+h, x:x+w]
+                img = img[x:x+w, y:h]
                 encoded_img = cv2.imencode('.jpg', img)[1]
                 encoded_img = base64.b64encode(encoded_img).decode()
 
@@ -35,15 +36,18 @@ def stream_faces(ws):
                 "origin": "na porta do laboratório",
                 "image": encoded_img
             }
-            ws.send(json.dumps(data))
 
-        # cv2.imshow('image', img)
-        # k = cv2.waitKey(10) & 0xff
+            if int(redis_instance.get('flag')) == 1:
+                redis_instance.set('flag', 1)
+                ws.send(json.dumps(data))
 
-        time.sleep(1)
+        cv2.imshow('image', img)
+        k = cv2.waitKey(100) & 0xff
 
-        # if k == 27:
-        # break
+        if k == 27:
+            break
+
+        time.sleep(0.5)
 
     cap.release()
     cv2.destroyAllWindows()
